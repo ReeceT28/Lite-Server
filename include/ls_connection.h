6db38a5fcaeafc499d6926bd799e6fa59ddb6995
reecet <reecesamueltaylor@gmail.com>
2026-03-05 17:27:21 +0000

    Small changes - just pushing to desktop from laptop

--- include/ls_connection.h
+++ include/ls_connection.h
@@ -3,15 +3,15 @@
 #include <sys/socket.h>
 
 /* Represents a listening socket, essentially our server */
-typedef struct ls_sock_lstn_s { 
-    int fd;
-    struct sockaddr *sockaddr; 
-    socklen_t socklen; 
-} ls_server_t;
+typedef struct ls_lstning_sock_s {
+  int fd;
+  struct sockaddr *sockaddr;
+  socklen_t socklen;
+} ls_lstning_sock_t;
 
 /* Represents a connection (a client) */
 typedef struct ls_connection_s {
-    int fd;
-    ls_event_t* read_event;
-    ls_event_t* write_event; 
+  int fd;
+  ls_event_t *read_event;
+  ls_event_t *write_event;
 } ls_connection_t;
--- src/main.c
+++ src/main.c
@@ -1,17 +1,20 @@
+#include "ls_http_parser.h"
 #include <netinet/in.h>
+#include <sys/epoll.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include "ls_http_parser.h"
 
-int main()
-{
-    ls_http_parser_init();
-    return 0;
+int main() {
+  ls_http_parser_init();
+  int epfd = epoll_create1(0);
+
+  return 0;
 }
 
 /*
  * NEXT STEPS:
- * Prevent Directory Traversal by using more secure functions like openat2(), can take inspiration from
+ * Prevent Directory Traversal by using more secure functions like openat2(),
+ * can take inspiration from
  * https://github.com/google/safeopen/blob/66b54d5181c6fee7ceb20782daca922b24f62819/safeopen_linux.go
  * Handle multiple clients, look into Having Open connections
  * Support more methods like POST
